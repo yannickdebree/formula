c32c1a6269d9d67f39e8a1e2b0230241a7cd5101
Yannick Debrée <contact@yannickdebree.com>
2022-08-23 16:13:47 +0200

    Style isolated thanks Vuejs

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
+import vue from '@vitejs/plugin-vue';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  plugins: [vue()],
   root: 'src',
   server: { port: 8080 },
   build: {
