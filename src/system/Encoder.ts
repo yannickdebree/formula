8ae637ae345f762296c24d30321279287b0d3b7f
Yannick Debrée <contact@yannickdebree.com>
2022-09-08 00:27:45 +0200

    Window injected in Encoder scope

--- src/system/Encoder.ts
+++ src/system/Encoder.ts
@@ -1,9 +1,14 @@
+import { Inject } from './di';
+
+@Inject(Window)
 export class Encoder {
+  constructor(private readonly window: Window) {}
+
   encode(data: string) {
-    return btoa(data);
+    return this.window.btoa(data);
   }
 
   decode(data: string) {
-    return atob(data);
+    return this.window.atob(data);
   }
 }
