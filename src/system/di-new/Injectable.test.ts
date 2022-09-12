735e9dbe20c737898788ea3f30665f03ca594f94
Yannick Debrée <Yannick.DEBREE.external@stormshield.eu>
2022-09-12 13:51:17 +0200

    di in progress

--- src/system/di-new/Injectable.test.ts
+++ src/system/di-new/Injectable.test.ts
@@ -1,44 +1,99 @@
-import { Container } from './Container';
-import { ProviderNotFoundError } from './errors';
-import { Injectable } from './Injectable';
+import 'reflect-metadata';
 
-class UnsavedClass {}
+// import { Container } from './Container';
+// import { ProviderNotFoundError } from './errors';
+// import { Injectable } from './Injectable';
 
-@Injectable()
-class SavedClassA {}
+// class UnsavedClass {}
 
-@Injectable()
-class SavedClassB {
-  constructor(public readonly savedClassA: SavedClassA) {}
+// @Injectable()
+// class SavedClassA {}
+
+// @Injectable()
+// class SavedClassB {
+//   constructor(public readonly savedClassA: SavedClassA) {}
+// }
+
+// let container: Container;
+
+// describe(Injectable.name, () => {
+//   beforeEach(() => {
+//     container = new Container();
+//   });
+
+//   it('Cannot get unsaved class instance', () => {
+//     try {
+//       container.get(UnsavedClass);
+//     } catch (err) {
+//       expect(err).toBeInstanceOf(ProviderNotFoundError);
+//     }
+//   });
+
+//   it('Can save and get value by token', () => {
+//     const instance = container.get(SavedClassA);
+
+//     expect(instance).toBeDefined();
+//     expect(instance).toBeInstanceOf(SavedClassA);
+//   });
+
+//   it('Can save and get class instance with dependencies', () => {
+//     const instance = container.get<SavedClassB>(SavedClassB);
+
+//     expect(instance).toBeDefined();
+//     expect(instance).toBeInstanceOf(SavedClassB);
+
+//     expect(instance.savedClassA).toBeDefined();
+//     expect(instance.savedClassA).toBeInstanceOf(SavedClassA);
+//   });
+// });
+
+type GenericClassDecorator<T> = (target: T) => void;
+interface Type<T> {
+  new (...args: any[]): T;
 }
 
-let container: Container;
+const Service = (): GenericClassDecorator<Type<object>> => {
+  return (target) => {
+    console.log('test : ', Reflect.getMetadata('design:paramtypes', target));
+  };
+};
 
-describe(Injectable.name, () => {
-  beforeEach(() => {
-    container = new Container();
-  });
+class Injector {
+  // resolving instances
+  resolve<T>(target: Type<any>): T {
+    // tokens are required dependencies, while injections are resolved tokens from the Injector
+    const tokens = Reflect.getMetadata('design:paramtypes', target) || [];
+    console.log('tokens : ', tokens);
+    const injections = tokens.map((token: any) => this.resolve<any>(token));
 
-  it('Cannot get unsaved class instance', () => {
-    try {
-      container.get(UnsavedClass);
-    } catch (err) {
-      expect(err).toBeInstanceOf(ProviderNotFoundError);
-    }
-  });
+    return new target(...injections);
+  }
+}
 
-  it('Can save and get value by token', () => {
-    const instance = container.get(SavedClassA);
+@Service()
+class Foo {
+  doFooStuff() {
+    return 'foo';
+  }
+}
 
-    expect(instance).toBeDefined();
-    expect(instance).toBeInstanceOf(SavedClassA);
-  });
+@Service()
+class Bar {
+  constructor(public foo: Foo) {}
 
-  it('Can save and get class instance with dependencies', () => {
-    const instance = container.get<SavedClassB>(SavedClassB);
+  doBarStuff() {
+    return 'bar';
+  }
+}
 
-    expect(instance).toBeDefined();
-    expect(instance).toBeInstanceOf(SavedClassB);
-    expect(instance.savedClassA).toBeInstanceOf(SavedClassA);
+@Service()
+class Foobar {
+  constructor(public foo: Foo, public bar: Bar) {}
+}
+
+describe('DI', () => {
+  it('test', () => {
+    const foobar = new Injector().resolve<Foobar>(Foobar);
+    expect(foobar.bar.doBarStuff()).toEqual('bar');
   });
 });
